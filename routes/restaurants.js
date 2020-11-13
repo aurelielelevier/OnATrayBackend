a53c45b09785dcecea2614c7c2381220fac0f826
Aurélie LE LEVIER <aurelie.lelevier@yahoo.fr>
2020-11-13 21:36:16 +0100

    ajout signup

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,5 +3,55 @@ var router = express.Router();
 var talentModel = require('../model/talents')
 var restaurantModel = require('../model/restaurants')
 
+router.post('/recherche-liste-talents', async function(req,res,next){
+   var données= JSON.parse(req.body.criteres)
+    var restaurant = await restaurantModel.findOne({token:req.body.token})
+    var metier = [données.posterecherché]
+    var contrat = [données.typedecontrat]
+    console.log('CONTRATS',metier)
+    console.log('POSTES', contrat)
+    
+    // Permet de récupérer les talents à afficher en fonction des fitlres appliqués
+    // Condition avec toutes les postes
+            
+    var responseAenvoyer = await talentModel.find({
+        lookingJob:{$in:[données.posterecherché]},
+        typeofContract:{$in:[données.typedecontrat][0]},
+        polygone: {
+            $geoIntersects: {
+            $geometry: {
+                type: "Point" ,
+                coordinates: restaurant.adresselgtlat.coordinates,
+            }
+            }
+        }
+        }).populate('formation').populate('experience').exec()
+        console.log(('chargement avec tri',responseAenvoyer))
+            
+        let restaurantwishlistexpand = await restaurantModel.findOne({token:req.body.token}).populate('wishlistRestaurant').exec()
+        let restaurantwishlistid = await restaurantModel.findOne({token:req.body.token})
+  
+      res.json({liste:responseAenvoyer,restaurantwishlist:restaurantwishlistexpand,restaurantwishlistid:restaurantwishlistid.wishlistRestaurant})
+     })
+    
+     
+router.post('/wishlist', async function (req,res,next){
+var user = await restaurantModel.findOne({token: req.body.token})
+    var talent = await talentModel.findOne({_id: req.body.id})
+
+    if(user.wishlistRestaurant.includes(talent.id)){ 
+            await restaurantModel.updateOne({token: req.body.token}, { $pull: {wishlistRestaurant:{ $in:`${req.body.id}` }} })
+    console.log('retrait wishlist')  
+    await talentModel.findByIdAndUpdate(talent.id,{$inc:{countFave:-1,"metrics.orders": 1}})
+    } else {
+        await restaurantModel.updateOne({token: req.body.token}, {$addToSet:{ wishlistRestaurant:req.body.id}})
+        await talentModel.findByIdAndUpdate(talent.id,{$inc:{countFave:+1,"metrics.orders": 1}})
+    console.log('ajout wishlist')}
+
+    var responseAenvoyer=await talentModel.find().populate('formation').populate('experience').exec()
+    var restaurant= await restaurantModel.findOne({token:req.body.token})
+
+res.json({profil: restaurant,liste: responseAenvoyer})
+})
 
 module.exports = router;
--- routes/talents.js
+++ routes/talents.js
@@ -2,6 +2,66 @@ var express = require('express');
 var router = express.Router();
 var talentModel = require('../model/talents')
 var restaurantModel = require('../model/restaurants')
+const {request} = require('express');
+var uid2 = require('uid2');
+var SHA256 = require("crypto-js/sha256");
+var encBase64 = require("crypto-js/enc-base64");
+
+router.post('/createAccount', async function(req,res,next){
+  const zoneFrance= [
+    [ -5.3173828125, 48.458124202908934 ],
+    [ 2.1313476562500004, 51.26170001449684 ],
+    [ 8.811035156250002, 48.90783374365477 ],
+    [ 7.998046875000001, 43.70709714273101 ],
+    [ 3.2080078125000004, 42.228008913641865 ],
+    [ 1.4941406250000002, 42.293056273848215 ],
+    [ -2.0214843750000004, 43.06838615478111 ],
+    [ -5.3173828125, 48.458124202908934 ]
+  ]
+  var polygoneFrance = {
+    type: "Polygon" ,
+    coordinates: [
+      zoneFrance
+    ]
+  }
+    var salt = uid2(32)
+    var talentToCheck = await talentModel.findOne({email:req.body.talentEmail})
+    var avatar = 'https://res.cloudinary.com/dpyqb49ha/image/upload/v1604324805/mucu7fy5dbhrxmhtf1dc.jpg'
+    if(talentToCheck === null){
+      var newTalent = await new talentModel({
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        email : req.body.email,
+        password : SHA256(req.body.password + salt).toString(encBase64),
+        token: uid2(32), 
+        salt : salt,
+        phone : req.body.phone,
+        avatar:avatar,
+        lookingForJob:false,
+        working:false,
+        speakLangage:[],
+        adress:'',
+        adresselgtlat: {
+          type: "Point" ,
+          coordinates: [2.33,48.33]},
+        polygone: polygoneFrance,
+        perimetre :zoneFrance,
+        lookingJob:[],
+        typeofContract: [],
+        wishlistTalent:[],
+        experience:[],
+        formation:[],
+        chatRoom:[],
+        countFave:0,
+      })
+      var talentSaved = await newTalent.save();
+      if(talentSaved){
+        res.json({profil: talentSaved})
+      }else{
+        res.json(false)
+      }
+    }
+  })
 
 router.post(`/recherche-liste-restaurants`, async function(req, res, next){
   // tri dans la liste des restaurants à retourner en fonction des différents critères, vérifie si
@@ -59,4 +119,48 @@ router.post(`/recherche-liste-restaurants`, async function(req, res, next){
    res.json(user.wishlistTalent)
   });
 
+  router.post('/informations', async function(req,res,next){
+
+    dateFormat = function (date) {
+   
+      let day = ("0" + date.getDate()).slice(-2);
+      let month = ("0" + (date.getMonth() + 1)).slice(-2);
+      let year = date.getFullYear();
+      let dateFormat = day+"/"+month+"/"+year;
+      return dateFormat 
+    }
+    
+    var job = JSON.parse(req.body.job)
+    var langage = JSON.parse(req.body.langage)
+    var typeofContract=JSON.parse(req.body.contrat)
+  
+    await talentModel.updateOne({token:req.body.token},{speakLangage:langage, working:req.body.poste, lookingForJob: req.body.recherche, lookingJob:job, typeofContract:req.body.contrat})
+    
+    var formation = JSON.parse(req.body.formation)
+    var experience = JSON.parse(req.body.experience)
+  
+    for (let i=0;i<formation.length;i++){
+    var newFormation = await new formationModel({
+    school : formation[i].school,
+    diploma : formation[i].diploma,
+    endingDate : dateFormat(new Date(formation[i].year)),
+    city : formation[i].city
+    })
+    await newFormation.save();
+    await talentModel.updateOne({token:req.body.token},{$addToSet:{formation:newFormation.id}})
+    }
+    
+    for(let i=0; i<experience.length;i++){
+      var newExperience = await new experienceModel({
+      firm : experience[i].firm,
+      city : experience[i].city,
+      startingDate : dateFormat(new Date(experience[i].rangeDate[0])),
+      endingDate : dateFormat(new Date(experience[i].rangeDate[1])),
+      job: experience[i].job,
+      })
+    await newExperience.save();
+    await talentModel.updateOne({token:req.body.token},{$addToSet:{experience:newExperience.id}})
+    }
+  })
+  
   module.exports = router;
